54db7171123c3d5451e53198335fc4287a8f0bf2
X4lldux <x4lldux@vectron.io>
2016-09-27 15:03:06 +0200

    Add support for subscribing to all events

--- lib/neuryt/event_bus.ex
+++ lib/neuryt/event_bus.ex
@@ -4,11 +4,28 @@ defmodule Neuryt.EventBus do
   """
 
   @doc """
-  Subscribes process for events of specific type.
+  Publishes an event to all it subscribers.
+  """
+  @spec publish(Neuryt.Event.t) :: :ok
+  def publish(%Neuryt.Event{event: %{__struct__: event_name}}=event) do
+    :pg2.create all_events_group
+    :pg2.create event_group(event_name)
+
+    [all_events_group, event_group(event_name)]
+    |> Enum.flat_map(&:pg2.get_local_members/1)
+    |> Enum.uniq
+    |> Enum.each(fn x -> send x, event end)
+
+    :ok
+  end
+
+
+  @doc """
+  Subscribes processes for events of specific type.
   """
   @spec subscribe(any) :: :ok
   def subscribe(event_name) do
-    event_group = {:event, event_name}
+    event_group = event_group(event_name)
     :pg2.create event_group
     if not self in :pg2.get_local_members event_group do
       :pg2.join event_group, self
@@ -17,23 +34,15 @@ defmodule Neuryt.EventBus do
     :ok
   end
 
+  @doc """
+  Unsubscribes from an event.
+  """
+  @spec unsubscribe(Neuryt.Event.t) :: :ok
   def unsubscribe(event_name) do
-    event_group = {:event, event_name}
+    event_group = event_group(event_name)
     if self in :pg2.get_local_members event_group do
       :pg2.leave event_group, self
     end
-    :ok
-  end
-
-  @doc """
-  Publishes an event to all it subscribers.
-  """
-
-  @spec publish(Neuryt.Event.t) :: :ok
-  def publish(%Neuryt.Event{event: %{__struct__: event_name}}=event) do
-    {:event, event_name}
-    |> :pg2.get_local_members
-    |> Enum.each(fn x -> send x, event end)
 
     :ok
   end
@@ -43,6 +52,44 @@ defmodule Neuryt.EventBus do
   """
   @spec list_subscribers(any) :: [pid]
   def list_subscribers(event_name) do
-    :pg2.get_local_members {:event, event_name}
+    :pg2.get_local_members event_group(event_name)
   end
+
+  @doc """
+  Subscribes to all events that go through event bus.
+  """
+  @spec subscribe_to_all_events() :: :ok
+  def subscribe_to_all_events do
+    :pg2.create all_events_group
+    if not self in :pg2.get_local_members all_events_group do
+      :pg2.join all_events_group, self
+    end
+
+    :ok
+  end
+
+  @doc """
+  List all processes subscribed to the special `all events` stream.
+  """
+  @spec list_subscribers_to_all_events :: [pid]
+  def list_subscribers_to_all_events do
+    :pg2.create all_events_group
+    :pg2.get_local_members all_events_group
+  end
+
+  @doc """
+  Unsubscribes from special `all events` stream.
+  """
+  @spec unsubscribe_from_all_events :: :ok
+  def unsubscribe_from_all_events do
+    if self in :pg2.get_local_members all_events_group do
+      :pg2.leave all_events_group, self
+    end
+
+    :ok
+  end
+
+
+  defp event_group(event_name), do: {__MODULE__, :event, event_name}
+  defp all_events_group, do: {__MODULE__, :all_events}
 end
--- test/event_bus_test.exs
+++ test/event_bus_test.exs
@@ -5,7 +5,7 @@ defmodule SomeEvents do
 end
 
 defmodule EventBusTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   require SomeEvents
 
   alias Neuryt.{Event, EventBus}
@@ -49,4 +49,36 @@ defmodule EventBusTest do
     assert [] == EventBus.list_subscribers(SomeEvents)
   end
 
+
+  test "can sunbscrinbe to special `all events` stream" do
+    EventBus.subscribe_to_all_events
+    assert [self] == EventBus.list_subscribers_to_all_events
+  end
+
+  test "when publishing an event it also is published to special `all events` stream" do
+    home = self
+    receiver_fun = fn ->
+      EventBus.subscribe_to_all_events
+      send home, :ready
+      assert_receive %Event{event: SomeEvents.c Event1}, 1000
+      send home, :done
+    end
+
+    spawn_link receiver_fun
+    assert_receive :ready
+    spawn_link receiver_fun
+    assert_receive :ready
+
+    EventBus.publish %Event{event: SomeEvents.c Event1}
+
+    assert_receive :done
+    assert_receive :done
+  end
+
+  test "can unsubscribe from special `all events` stream" do
+    EventBus.subscribe_to_all_events
+    assert [self] == EventBus.list_subscribers_to_all_events
+    EventBus.unsubscribe_from_all_events
+    assert [] == EventBus.list_subscribers_to_all_events
+  end
 end
